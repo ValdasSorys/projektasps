4aa94f5ab48c865fa4bee5288faeee3c5e57a141
ValdasSorys <47417153+ValdasSorys@users.noreply.github.com>
2020-04-22 19:32:12 +0300

    Initial

---
+++ WebApplication6/WebApplication6/Controllers/HomeController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication6.Models;
+
+namespace WebApplication6.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = new band1(1);
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ WebApplication6/WebApplication6/Models/band1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using MySql.Data;
+using System.Configuration;
+using MySql.Data.MySqlClient;
+
+namespace WebApplication6.Models
+{
+    public class band1
+    {
+        public int id { get; set; }
+        public string pavadinimas { get; set; }
+
+        string connStr = ConfigurationManager.ConnectionStrings["myConnectionString"].ConnectionString;
+        public band1()
+        {
+            id = 1;
+            pavadinimas = connStr;
+        }
+
+        public band1(int _id)
+        {
+            var con = new MySqlConnection(connStr);
+            con.Open();
+            string sql = "SELECT * FROM band1 WHERE id = " + _id;
+            var cmd = new MySqlCommand(sql, con);
+
+            MySqlDataReader rdr = cmd.ExecuteReader();
+            cmd = new MySqlCommand(sql, con);
+            rdr.Read();
+            if (rdr.HasRows)
+            {
+                id = rdr.GetInt32(0);
+                pavadinimas = rdr.GetString(1);
+            }
+            else
+            {
+                id = -1;
+                pavadinimas = "nerado";
+            }
+            rdr.Close();
+            con.Close();
+        }
+    }
+}
